056fb7722901cc3369f4de0efbe615fd471fac98
Bikash864 <bikash.kumar86469@gmail.com>
2026-03-25 01:06:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,15 @@
 const CACHE_NAME = "landcalc-v2";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("activate", e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
